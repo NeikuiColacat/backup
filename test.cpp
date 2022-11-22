144ebc1973f350887375a3d245be716b5bf9c663
colacat neikui <neikui.colacat@gmail.com>
2022-11-22 23:03:37 +0800

    s

--- test.cpp
+++ test.cpp
@@ -7,22 +7,24 @@ int n, m;
 const int N = 1e6;
 int ne[N], e[N], h[N], idx,cnt[N];
 queue<int> q;
+queue<int> temp;
 void add(int a, int b) {
 	e[idx] = b, ne[idx] = h[a], h[a] = idx++;
 }
 bool topsort() {
 	for (int i = 1; i <= n; i++) {
-		if (!cnt[i])q.push(i);
+		if (!cnt[i])q.push(i),temp.push(i);
 	}
 	while (q.size()) {
 		auto t = q.front();
+		q.pop();
 		for (int i = h[t]; i != -1; i = ne[i]) {
-			int j = ne[i];
+			int j = e[i];
 			cnt[j]--;
-			if (cnt[j] == 0)q.push(j);
+			if (cnt[j] == 0)q.push(j),temp.push(j);
 		}
 	}
-	return q.size() == n - 1;
+	return temp.size() == n - 1;
 }
 signed main() {
 	cin >> n >> m;
@@ -34,7 +36,7 @@ signed main() {
 		cnt[y]++;
 	}
 	if (topsort()) {
-		while (q.size())cout << q.front() << ' ', q.pop();
+		while (temp.size())cout << temp.front() << ' ', temp.pop();
 		cout << '\n';
 	}
 	else cout << "-1\n";
